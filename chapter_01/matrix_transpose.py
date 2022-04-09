2e62f7c691c867aeaa47d9db31b6aedfa42b5c8a
nirojshrestha019 <nirojshrestha019@gmail.com>
2022-04-09 16:50:12 -0300

    martrix transpose pythonic way added

--- chapter_01/matrix_transpose.py
+++ chapter_01/matrix_transpose.py
@@ -2,8 +2,13 @@
 # l = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 # r = [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
 
+# classic way
+def transpose(m):
+    for i in range(1, len(m)):
+        for j in range(i):
+            m[i][j], m[j][i] = m[j][i], m[i][j]
 
-
+# pythonic way
 def transpose_matrix(matrix):
     return list(map(list, zip(*matrix)))
 
